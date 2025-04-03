e5503032e6aee9780a79f1746c5e7e67611809df
Vaishnavi-balaji <168355781+Vaishnavi-balaji@users.noreply.github.com>
2025-04-03 13:52:18 +0200

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args)
+	{
+		System.out.println("CHROME");
+		float version=91.0f;
+		System.out.println("Version=" + version);
+		
+	    String developer="Google";
+	    System.out.println("Developer=" + developer);
+	    
+	    boolean isBeta=false;
+	    System.out.println("Beta=" + isBeta);
+	    
+	    int releaseYear=2008;
+	    System.out.println("Release Year=" + releaseYear);
+	    
+	    char shortcutKey='C';
+	    System.out.println("Shortcutkey=" +
+	    shortcutKey);
+				
+		
+	}
+
+}
